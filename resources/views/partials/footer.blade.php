3f66256a5cf99740254235dde7aa52781710fc5b
RooRsmoka <chri.disa96@gmail.com>
2022-03-03 20:52:58 +0100

    created default layout with static header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,81 @@
+<footer>
+    <div class="footer-up">
+        <div class="container lists">
+            <div class="footer-list">
+                <div class="list">
+                    <p class="list-title">dc comics</p> 
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <p class="list-title">shop</p> 
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <p class="list-title">dc</p> 
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Rating</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <p class="list-title">sites</p> 
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-logo">
+                <img src="{{ asset('img/dc-logo-bg.png') }}">
+            </div>
+        </div>
+    </div>
+    <div class="footer-down">
+        <div class="container contact">
+            <div class="sign-up-btn">
+                sign-up now!
+            </div>
+            <div class="social">
+                <p>follow us</p>
+                <a href="">
+                    <img src="{{ asset('img/footer-facebook.png') }}" alt="Facebook">
+                </a>
+                <a href="">
+                    <img src="{{ asset('img/footer-twitter.png') }}" alt="Twitter">
+                </a>
+                <a href="">
+                    <img src="{{ asset('img/footer-youtube.png') }}" alt="Youtube">
+                </a>
+                <a href="">
+                    <img src="{{ asset('img/footer-pinterest.png') }}" alt="Pinterest">
+                </a>
+                <a href="">
+                    <img src="{{ asset('img/footer-periscope.png') }}" alt="Periscope">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="container header">
+        <div class="logo">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </a>
+        </div>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="{{ route('characters') }}">characters</a></li>
+                <li><a href="{{ route('comics') }}">comics</a></li>
+                <li><a href="{{ route('movies') }}">movies</a></li>
+                <li><a href="{{ route('tv') }}">tv</a></li>
+                <li><a href="{{ route('games') }}">games</a></li>
+                <li><a href="{{ route('collectibles') }}">collectibles</a></li>
+                <li><a href="{{ route('videos') }}">videos</a></li>
+                <li><a href="{{ route('fans') }}">fans</a></li>
+                <li><a href="{{ route('news') }}">news</a></li>
+                <li><a href="{{ route('shop') }}">shop</a></li>
+            </ul>
+        </div>
+        <input type="text">
+    </div>
+</header>
